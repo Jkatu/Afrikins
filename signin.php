af45d22ad3a31bd5e78d0ae8732b85bbf4fd0aff
Jkatu <141175580+Jkatu@users.noreply.github.com>
2025-08-05 03:27:56 +0300

    added

--- signin.php
+++ signin.php
@@ -35,6 +35,7 @@
         <p>
             We are committed to truthful storytelling that amplifies African voices, preserves heritage, and challenges stereotypes. 
             Every project we take on is handled with professionalism, cultural respect, and a dedication to excellence.
+            We are 
         </p>
     </div>
 
