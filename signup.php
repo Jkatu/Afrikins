52a07d9ca88b61e4e67905542229596ec15a2046
Jkatu <141175580+Jkatu@users.noreply.github.com>
2025-08-05 20:58:10 +0300

    signup

--- signup.php
+++ signup.php
@@ -279,7 +279,7 @@ while ($row = $rRes->fetchArray(SQLITE3_ASSOC)) {
             <li>Early and easy access to film cuts</li>
             <li>Invitation to collaborative research</li>
             <li>Credits on projects you contribute to</li>
-            <li>Unlimited access to document </li>
+            <li>Unlimited access to documentaries </li>
           </ul>
         </div>
       </div>
