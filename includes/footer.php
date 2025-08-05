2bc7150b7deebd066bb07b2e75fa70ff0f65b960
Jkatu <141175580+Jkatu@users.noreply.github.com>
2025-08-05 05:29:21 +0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <p>&copy; <?php echo date("Y"); ?> Afrikins. All rights reserved.</p>
-    <p>Contact us at <a href="Afrikins:@yahoo.com">dbt@yahoo.com</a></p>
+    <p>Contact us at <a href="Afrikins:@gmaioo.com">dbt@yahoo.com</a></p>
 </div>
 </body>
 </html>
